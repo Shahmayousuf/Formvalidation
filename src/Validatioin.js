cedd80a21ef2d7a84ebebe26e7da81401f1a69cf
Shahmayousuf <shahmayousuf@gmail.com>
2023-05-18 16:21:24 +0530

    yup library

--- src/Validatioin.js
+++ src/Validatioin.js
@@ -1,32 +1,28 @@
 import React from "react";
 import { useFormik } from "formik";
-import * as Yup from 'yup'
+import * as Yup from "yup";
 
 const Validatioin = () => {
-
-
-  const initialValues={
-    
-      name: "",
-      email: "",
-      password: "",
-    
-  }
-  const   onSubmit =values=>{
-    console.log('form data',values);
-  }
-  const  validationSchema=Yup.object({
-    name:Yup.string().required('Required!'),
-    email:Yup.string().email('invalid email format').required('Required!'),
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
+  const onSubmit = (values) => {
+    console.log("form data", values);
+  };
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required!"),
+    email: Yup.string().email("invalid email format").required("Required!"),
     password: Yup.string()
       .matches(
         /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
         "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
       )
-      .required("Required!")
-  })
+      .required("Required!"),
+  });
   // const validate=values=>{
-  //   //values.name values  .email values.channel 
+  //   //values.name values  .email values.channel
   //   let errors={}
   //   if(!values.name){
   //     errors.name="Required"
@@ -42,66 +38,71 @@ const Validatioin = () => {
   //   ){
   //     errors.password='invalid password'
   //   }
-   
+
   //   return errors
   // }
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validationSchema
+    validationSchema,
     // validate,
   });
-  console.log('formvalues',formik.values);
-  console.log('errors',formik.errors); 
-  console.log("visited fields",formik.touched);
+  console.log("formvalues", formik.values);
+  console.log("errors", formik.errors);
+  console.log("visited fields", formik.touched);
   return (
     <div className="body">
       <h2 style={{ color: "white" }}>Login</h2>
       <div>
         <form onSubmit={formik.handleSubmit}>
           <div className="form-control">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.name}
-          />
-          { formik.touched.name && formik.errors.name?(
-          <div className="error">{formik.errors.name}</div>
-          ):null}
-          
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              // onChange={formik.handleChange}
+              // onBlur={formik.handleBlur}
+              // value={formik.values.name}
+              {...formik.getFieldProps("name")}
+            />
+            {formik.touched.name && formik.errors.name ? (
+              <div className="error">{formik.errors.name}</div>
+            ) : null}
           </div>
           <div className="form-control">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-                    {formik.errors.email?(
-          <div className="error">{ formik.touched.email&&formik.errors.email}</div>
-          ):null}
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              // onChange={formik.handleChange}
+              // onBlur={formik.handleBlur}
+              // value={formik.values.email}
+              {...formik.getFieldProps("email")}
+            />
+            {formik.errors.email ? (
+              <div className="error">
+                {formik.touched.email && formik.errors.email}
+              </div>
+            ) : null}
           </div>
           <div className="form-control">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-          />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            />
           </div>
-          {formik.errors.password?(
-          <div className="error">{ formik.touched.password && formik.errors.password}</div>
-          ):null}
+          {formik.errors.password ? (
+            <div className="error">
+              {formik.touched.password && formik.errors.password}
+            </div>
+          ) : null}
           <button type="submit">Submit</button>
         </form>
       </div>
